85bad0dd66b23ead679e3ac74429a78b734dcda9
longhirar <longhirar@gmail.com>
2022-07-10 20:08:08 -0300

    Add languages, reworked index, character page (beta) and error page.

---
+++ language/en_US.js
@@ -0,0 +1,12 @@
+const lang = {
+    "index.slogan": "Create and manage your Dungeons & Dragons character for free.",
+    "index.createnew": "+ CREATE NEW",
+    "index.load": "⇪ LOAD CHARACTER",
+
+    "character.creation.title": "Character Creation",
+    "character.creation.create": "CREATE",
+
+    "character.classes.rogue": "Rogue",
+
+    "character.races.lightfoot_haffling": "Lightfoot Haffling"
+}
---
+++ language/pt_BR.js
@@ -0,0 +1,12 @@
+const lang = {
+    "index.slogan": "Crie e gerencie seu personagem do Dungeons & Dragons de graça.",
+    "index.createnew": "+ CRIAR NOVO",
+    "index.load": "⇪ CARREGAR",
+
+    "character.creation.title": "Criação de Personagem",
+    "character.creation.create": "CRIAR",
+
+    "character.classes.rogue": "Rogue",
+
+    "character.races.lightfoot_haffling": "Lightfoot Haffling"
+}
